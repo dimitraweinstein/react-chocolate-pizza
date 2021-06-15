8cba83ecf9090c8e0c820c2ea9d5da765636d6d8
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-15 00:06:17 -0700

    fixed app.js to be more clean and clear with what is to be rendered, added remaining copy from recipe directions

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -13,7 +13,13 @@ export default class Body extends Component {
                 <main>
                     <img src= 'choco-pizza.png' alt = 'main-pic' className = 'main-pic' />
                     <p>
-                        For the fig-swirl: Melt butter over medium heat in a saucepan. Add a 
+                        For the fig-swirl: Melt butter over medium heat in a saucepan. Add brown sugar and stir to dissolve. Halve all of the figs and toss in the saucepan with water and lemon juice. Cook over medium heat, stirring frequently, until you have a chunky-jammy mixture. Add salt with one or two stirs, set aside and let cool completely.
+                    </p>
+                    <p>
+                        Ice cream: In a small pot over medium heat, combine milk, and granulated sugar until sugar is completely dissolved and the milk is just barely lukewarm. Whisk in teh egg yolks. Set mixture in the fridge and wait until the fig mixture is cooled.
+                    </p>
+                    <p>
+                        Using an ice cream machine, pour liquids into the frozen basin and process according to manufacturer instructions, i.e., let spin and thicken for 10 minutes. Pour semi-frozen mixture into a pyrex dish or glass tupperware. Freeze for at least two hours before serving.
                     </p>
                 </main>
                 <section>
